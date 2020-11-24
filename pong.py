acf372ea57c3b83cf08092aa6f736909221c69cc
Shayn Adatia <Shayn@Sefras-MacBook-Air.local>
2020-11-24 18:25:27 -0500

    started working on moving ball

--- pong.py
+++ pong.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+import math
 
 # Define some colors
 BLACK = (0, 0, 0)
@@ -14,6 +16,26 @@ RECT_OFFSET_Y = 20
 RECT_WIDTH = 5
 RECT_HEIGHT = 50
 
+BALL_SIZE = 16
+
+class Ball:
+    """
+    Class to keep track of a ball's location and vector.
+    """
+    def __init__(self):
+        self.x = WIDTH/2
+        self.y = HEIGHT/2
+        self.speed = 1
+        self.direction = random.uniform(0, 1) * 2 * math.pi
+
+    def change_direction(self, direction):
+        self.direction = direction
+
+    def move_ip(self):
+        self.x += math.cos(self.direction) * self.speed
+        self.y += math.sin(self.direction) * self.speed
+  
+ 
 # Setup
 pygame.init()
  
@@ -40,11 +62,16 @@ right_y_speed = 0
 
 left_rect = pygame.Rect(RECT_OFFSET_X, RECT_OFFSET_Y, RECT_WIDTH, RECT_HEIGHT)
 right_rect = pygame.Rect(WIDTH - RECT_OFFSET_X, RECT_OFFSET_Y, RECT_WIDTH, RECT_HEIGHT)
+ball = Ball()
 
 pygame.display.flip()
 
 while not done:
     for event in pygame.event.get():
+
+        print(ball.x, ball.y)
+
+
         if event.type == pygame.QUIT:
             done = True
         
@@ -75,6 +102,7 @@ while not done:
         
     left_rect.move_ip(0, left_y_speed)
     right_rect.move_ip(0, right_y_speed)
+    ball.move_ip()
 
     left_rect.clamp_ip(SCREEN_BOUNDARIES)
     right_rect.clamp_ip(SCREEN_BOUNDARIES)
@@ -84,6 +112,7 @@ while not done:
 
     pygame.draw.rect(screen, WHITE, left_rect, 0)
     pygame.draw.rect(screen, WHITE, right_rect, 0)
+    pygame.draw.circle(screen, WHITE, [ball.x, ball.y], BALL_SIZE)
 
     pygame.display.flip()
 
